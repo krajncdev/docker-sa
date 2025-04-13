143b1a474a53d3b15ee85fdeaba97b5be9794073
Jaka Krajnc <jaka.krajnc.jk@gmail.com>
2025-04-13 18:06:03 +0200

    Chore: added tests and deployment CI CD

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import pytest
+import requests
+
+# Osnovni URL strežnika
+BASE_URL = "http://localhost:5001"
+
+# Test za končni toček /
+def test_hello_world():
+    response = requests.get(f"{BASE_URL}/")
+    assert response.status_code == 200
+    assert response.text == "Hello World"
